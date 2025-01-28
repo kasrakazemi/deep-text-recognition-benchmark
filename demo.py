41d35f7e03948d8144d83a6eb2bd43b4bac02abd
Kasra <55986800+kasrakazemi@users.noreply.github.com>
2025-01-28 21:23:34 +0330

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,7 +11,6 @@ from dataset import RawDataset, AlignCollate
 from model import Model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
 def demo(opt):
     """ model configuration """
     if 'CTC' in opt.Prediction:
@@ -43,7 +42,8 @@ def demo(opt):
 
     # predict
     model.eval()
-    with torch.no_grad():
+    output_file_path = './log_demo_result.txt'
+    with open(output_file_path, 'w') as log:  # Open in write mode
         for image_tensors, image_path_list in demo_loader:
             batch_size = image_tensors.size(0)
             image = image_tensors.to(device)
@@ -54,29 +54,21 @@ def demo(opt):
             if 'CTC' in opt.Prediction:
                 preds = model(image, text_for_pred)
 
-                # Select max probabilty (greedy decoding) then decode index to character
+                # Select max probability (greedy decoding) then decode index to character
                 preds_size = torch.IntTensor([preds.size(1)] * batch_size)
                 _, preds_index = preds.max(2)
-                # preds_index = preds_index.view(-1)
                 preds_str = converter.decode(preds_index, preds_size)
 
             else:
                 preds = model(image, text_for_pred, is_train=False)
 
-                # select max probabilty (greedy decoding) then decode index to character
+                # select max probability (greedy decoding) then decode index to character
                 _, preds_index = preds.max(2)
                 preds_str = converter.decode(preds_index, length_for_pred)
 
-
-            log = open(f'./log_demo_result.txt', 'a')
-            dashed_line = '-' * 80
-            head = f'{"image_path":25s}\t{"predicted_labels":25s}\tconfidence score'
-            
-            print(f'{dashed_line}\n{head}\n{dashed_line}')
-            log.write(f'{dashed_line}\n{head}\n{dashed_line}\n')
-
             preds_prob = F.softmax(preds, dim=2)
             preds_max_prob, _ = preds_prob.max(dim=2)
+
             for img_name, pred, pred_max_prob in zip(image_path_list, preds_str, preds_max_prob):
                 if 'Attn' in opt.Prediction:
                     pred_EOS = pred.find('[s]')
@@ -86,10 +78,10 @@ def demo(opt):
                 # calculate confidence score (= multiply of pred_max_prob)
                 confidence_score = pred_max_prob.cumprod(dim=0)[-1]
 
-                print(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}')
-                log.write(f'{img_name:25s}\t{pred:25s}\t{confidence_score:0.4f}\n')
+                # Write only image name, prediction, and confidence score
+                log.write(f"{img_name.split('/')[-1]}\t{pred}\t{confidence_score:.4f}\n")
 
-            log.close()
+    print(f"Results saved to {output_file_path}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
