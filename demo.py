fa890b65cc96ba46dafc1c36a2056703ae1f8fe0
Kasra <55986800+kasrakazemi@users.noreply.github.com>
2025-01-29 12:00:32 +0330

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import string
 import argparse
-
+import time
 import torch
 import torch.backends.cudnn as cudnn
 import torch.utils.data
@@ -31,7 +31,7 @@ def demo(opt):
     print('loading pretrained model from %s' % opt.saved_model)
     model.load_state_dict(torch.load(opt.saved_model, map_location=device))
 
-    # prepare data. two demo images from https://github.com/bgshih/crnn#run-demo
+    # prepare data
     AlignCollate_demo = AlignCollate(imgH=opt.imgH, imgW=opt.imgW, keep_ratio_with_pad=opt.PAD)
     demo_data = RawDataset(root=opt.image_folder, opt=opt)  # use RawDataset
     demo_loader = torch.utils.data.DataLoader(
@@ -47,22 +47,16 @@ def demo(opt):
         for image_tensors, image_path_list in demo_loader:
             batch_size = image_tensors.size(0)
             image = image_tensors.to(device)
-            # For max length prediction
             length_for_pred = torch.IntTensor([opt.batch_max_length] * batch_size).to(device)
             text_for_pred = torch.LongTensor(batch_size, opt.batch_max_length + 1).fill_(0).to(device)
 
             if 'CTC' in opt.Prediction:
                 preds = model(image, text_for_pred)
-
-                # Select max probability (greedy decoding) then decode index to character
                 preds_size = torch.IntTensor([preds.size(1)] * batch_size)
                 _, preds_index = preds.max(2)
                 preds_str = converter.decode(preds_index, preds_size)
-
             else:
                 preds = model(image, text_for_pred, is_train=False)
-
-                # select max probability (greedy decoding) then decode index to character
                 _, preds_index = preds.max(2)
                 preds_str = converter.decode(preds_index, length_for_pred)
 
@@ -75,11 +69,10 @@ def demo(opt):
                     pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
                     pred_max_prob = pred_max_prob[:pred_EOS]
 
-                # calculate confidence score (= multiply of pred_max_prob)
                 confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+                extraction_time = int(time.time())  # Unix timestamp
 
-                # Write only image name, prediction, and confidence score
-                log.write(f"{img_name.split('/')[-1]}\t{pred}\t{confidence_score:.4f}\n")
+                log.write(f"{img_name.split('/')[-1]}\t{pred}\t{confidence_score:.4f}\t{extraction_time}\n")
 
     print(f"Results saved to {output_file_path}")
 
@@ -89,7 +82,6 @@ if __name__ == '__main__':
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
     parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
     parser.add_argument('--saved_model', required=True, help="path to saved_model to evaluation")
-    """ Data processing """
     parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
     parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
     parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
@@ -97,7 +89,6 @@ if __name__ == '__main__':
     parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
     parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
     parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
-    """ Model Architecture """
     parser.add_argument('--Transformation', type=str, required=True, help='Transformation stage. None|TPS')
     parser.add_argument('--FeatureExtraction', type=str, required=True, help='FeatureExtraction stage. VGG|RCNN|ResNet')
     parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
@@ -110,7 +101,6 @@ if __name__ == '__main__':
 
     opt = parser.parse_args()
 
-    """ vocab / character number configuration """
     if opt.sensitive:
         opt.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
 
